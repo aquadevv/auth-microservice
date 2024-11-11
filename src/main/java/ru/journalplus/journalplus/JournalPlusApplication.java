110d8e803d0b24977a67f1558265bf3b94ccea34
aquadevv <bonoberkut@gmail.com>
2024-11-11 20:38:15 +0300

    Initial project

---
+++ src/main/java/ru/journalplus/journalplus/JournalPlusApplication.java
@@ -0,0 +1,13 @@
+package ru.journalplus.journalplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/journalplus/journalplus/JournalPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.journalplus.journalplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
