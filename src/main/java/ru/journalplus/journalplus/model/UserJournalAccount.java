0f1c791472fbb673c82c2e9283da1afdadacb8bd
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-14 01:38:14 +0300

    ?? add @Getter & @Setter && add valid field

--- src/main/java/ru/journalplus/journalplus/model/UserJournalAccount.java
+++ src/main/java/ru/journalplus/journalplus/model/UserJournalAccount.java
@@ -1,9 +1,13 @@
 package ru.journalplus.journalplus.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Entity
+@Getter
+@Setter
 @Table(name = "user_journal_account")
 public class UserJournalAccount {
     @Id
@@ -12,4 +16,6 @@ public class UserJournalAccount {
 
     private String username;
     private String password;
+    private boolean valid;
+
 }
