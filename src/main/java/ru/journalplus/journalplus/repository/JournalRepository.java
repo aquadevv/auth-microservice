cbd3f18d419ce395134d73fb98805188864a2a47
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-14 20:45:24 +0300

    🐘 Create JournalRepository CRUD

---
+++ src/main/java/ru/journalplus/journalplus/repository/JournalRepository.java
@@ -0,0 +1,7 @@
+package ru.journalplus.journalplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.journalplus.journalplus.model.UserJournalAccount;
+
+public interface JournalRepository extends JpaRepository<UserJournalAccount, Long> {
+}
