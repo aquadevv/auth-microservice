1a67e79d3142f2c7f2095824e0381af5199161ad
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-14 20:04:32 +0300

    🐘 Add fields to UserMessengerAccount model & add Getter ; Setter

--- src/main/java/ru/journalplus/journalplus/model/UserMessengerAccount.java
+++ src/main/java/ru/journalplus/journalplus/model/UserMessengerAccount.java
@@ -1,2 +1,17 @@
-package ru.journalplus.journalplus.model;public class UserMessengerAccount {
+package ru.journalplus.journalplus.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_messenger_accounts")
+public class UserMessengerAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long UserMessengerId;
 }
