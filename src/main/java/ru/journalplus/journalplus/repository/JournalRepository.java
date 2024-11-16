7f5d782a0c4bf141bf68528140861d167177b6ed
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-16 17:17:35 +0300

    🐘 Add method for FindByUsername

--- src/main/java/ru/journalplus/journalplus/repository/JournalRepository.java
+++ src/main/java/ru/journalplus/journalplus/repository/JournalRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.journalplus.journalplus.model.UserJournalAccount;
 
 public interface JournalRepository extends JpaRepository<UserJournalAccount, Long> {
+    UserJournalAccount findByUsername(String username);
 }
