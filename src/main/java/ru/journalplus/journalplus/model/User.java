f26dbbde27370371fbf8ce04813ecf9978554bdc
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-17 03:32:12 +0300

    🎀 Add Setter and Getter

--- src/main/java/ru/journalplus/journalplus/model/User.java
+++ src/main/java/ru/journalplus/journalplus/model/User.java
@@ -1,9 +1,13 @@
 package ru.journalplus.journalplus.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Entity
+@Setter
+@Getter
 @Table(name = "users")
 public class User {
     @Id
