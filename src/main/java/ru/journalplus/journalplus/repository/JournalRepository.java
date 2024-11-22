59eed72bcf9113afaa724ca2baa4fb6159dabb5b
kanaok1 <stepanenkoewan@yandex.ru>
2024-11-22 20:48:41 +0300

    👓 Delete not used method in repository

--- src/main/java/ru/journalplus/journalplus/repository/JournalRepository.java
+++ src/main/java/ru/journalplus/journalplus/repository/JournalRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.journalplus.journalplus.model.UserJournalAccount;
 
 public interface JournalRepository extends JpaRepository<UserJournalAccount, Long> {
-    UserJournalAccount findByUsername(String username);
 }
