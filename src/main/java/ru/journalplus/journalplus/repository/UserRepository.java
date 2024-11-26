d40a4cfa763ad1bac74f060b715145fcee94b615
aquadev <bonoberkut@gmail.com>
2024-11-26 21:41:26 +0300

    👤 Update UserRepository: add find by username, exists by username

--- src/main/java/ru/journalplus/journalplus/repository/UserRepository.java
+++ src/main/java/ru/journalplus/journalplus/repository/UserRepository.java
@@ -3,5 +3,10 @@ package ru.journalplus.journalplus.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.journalplus.journalplus.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
