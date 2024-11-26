f576492fc53f2de338d3d986b7808969a3b583a7
aquadev <bonoberkut@gmail.com>
2024-11-26 21:43:02 +0300

    💻 New table for refresh token: create hibernate model + migration

---
+++ src/main/java/ru/journalplus/journalplus/model/RefreshToken.java
@@ -0,0 +1,29 @@
+package ru.journalplus.journalplus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "refresh_token")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiryDate;
+}
