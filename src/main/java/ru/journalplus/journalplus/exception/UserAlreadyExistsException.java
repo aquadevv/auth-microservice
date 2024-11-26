cedc0f835a3e320f745bf04644fdd8e420825bba
aquadev <bonoberkut@gmail.com>
2024-11-26 21:36:50 +0300

    ⚠️ Update custom exception UserAlreadyExistsException: add status

--- src/main/java/ru/journalplus/journalplus/exception/UserAlreadyExistsException.java
+++ src/main/java/ru/journalplus/journalplus/exception/UserAlreadyExistsException.java
@@ -1,11 +1,16 @@
 package ru.journalplus.journalplus.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
+@Getter
 public class UserAlreadyExistsException extends RuntimeException {
+    private final String status;
+
     public UserAlreadyExistsException(String message) {
         super(message);
+        this.status = "USER_ALREADY_EXISTS";
     }
 }
